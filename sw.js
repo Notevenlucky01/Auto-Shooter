ce376b2eebe4add79478023d78e28d02410ad82a
Notevenlucky01 <luc.schwenning@gmail.com>
2025-08-10 17:26:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'auto-shooter-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(request).then(cached => cached || fetch(request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
